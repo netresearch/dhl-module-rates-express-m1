ef67f26c3991066931fe396003f23a2a3b84b745
Andreas Müller <andreas.mueller@netresearch.de>
2018-10-29 17:14:01 +0100

    DHLEX-16: add source model international products

---
+++ src/app/code/community/Dhl/ExpressRates/Model/Adminhtml/System/Config/Source/InternationalProducts.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * See LICENSE.md for license details.
+ */
+use Dhl\Express\Api\Data\ShippingProductsInterface;
+
+/**
+ * Class Dhl_ExpressRates_Model_Adminhtml_System_Config_Source_InternationalProducts
+ *
+ * @package Dhl\ExpressRates\Model\Adminhtml
+ * @author  Andreas Müller <andreas.mueller@netreseach.de>
+ * @license https://opensource.org/licenses/osl-3.0.php Open Software License (OSL 3.0)
+ * @link    https://www.netresearch.de/
+ */
+class Dhl_ExpressRates_Model_Adminhtml_System_Config_Source_InternationalProducts
+{
+    const DELIMITER = ';';
+
+    /**
+     * @inheritdoc
+     */
+    public function toOptionArray()
+    {
+        $options = ShippingProductsInterface::PRODUCT_NAMES_INTERNATIONAL;
+
+        return array_map(
+            function ($label, $value) {
+                $value = implode(self::DELIMITER, $value);
+                return array(
+                    'value' => $value,
+                    'label' => $label,
+                );
+            },
+            array_keys($options),
+            $options
+        );
+    }
+}
