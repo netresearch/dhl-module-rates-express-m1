8b29af6513055a800ab32d5f1e57e3960a2d4154
Andreas Müller <andreas.mueller@netresearch.de>
2018-10-30 12:38:12 +0100

    DHLEX-14: Trigger rate request, add logger

--- src/app/code/community/Dhl/ExpressRates/Model/Carrier/Express.php
+++ src/app/code/community/Dhl/ExpressRates/Model/Carrier/Express.php
@@ -23,11 +23,82 @@ class Dhl_ExpressRates_Model_Carrier_Express
     protected $_code = self::CODE;
 
     /**
+     * @var Dhl_ExpressRates_Model_Config
+     */
+    protected $moduleConfig;
+
+    /**
+     * @var Dhl_ExpressRates_Model_Logger_Writer
+     */
+    protected $logWriter;
+
+    /**
+     * Dhl_ExpressRates_Model_Carrier_Express constructor.
+     */
+    public function __construct()
+    {
+        parent::__construct();
+        $this->moduleConfig = Mage::getModel('dhl_expressrates/config');
+        $this->logWriter = Mage::getModel('dhl_expressrates/logger_writer');
+    }
+
+
+    /**
      * @inheritDoc
      */
     public function collectRates(Mage_Shipping_Model_Rate_Request $request)
     {
-        return false;
+        if (!$this->getConfigFlag('active')) {
+            return false;
+        }
+
+        $store = $request->getStoreId();
+        $username = $this->moduleConfig->getUserName($store);
+        $password = $this->moduleConfig->getPassword($store);
+        $accountNumber = $this->moduleConfig->getAccountNumber($store);
+
+        $logger = new Dhl_ExpressRates_Model_Logger_Mage($this->logWriter);
+        $factory = new Dhl\Express\Webservice\SoapServiceFactory();
+        $client = $factory->createRateService($username, $password, $logger);
+
+        //create dummy request
+        $date = new DateTime('now');
+        $readyTime = $date->modify('+1 day');
+        $requestBuilder = new Dhl\Express\RequestBuilder\RateRequestBuilder();
+
+        $requestBuilder->setShipperAccountNumber($accountNumber);
+        $requestBuilder->setRecipientAddress(
+            $request->getDestCountryId(),
+            $request->getDestPostcode(),
+            $request->getDestCity(),
+            array(substr($request->getDestStreet(), 0, 35))
+        );
+        $requestBuilder->setShipperAddress(
+            $request->getCountryId(),
+            $request->getPostcode(),
+            $request->getCity()
+        );
+        $requestBuilder->setIsUnscheduledPickup(true);
+        $requestBuilder->setTermsOfTrade('');
+        $requestBuilder->addPackage('123456', $request->getPackageWeight(),'KG', '20', '10', '10','CM', true);
+        $requestBuilder->setContentType('NON_DOCUMENTS');
+        $requestBuilder->setReadyAtTimestamp($readyTime);
+        $requestBuilder->setIsValueAddedServicesRequested(false);
+        $requestBuilder->setNextBusinessDayIndicator(false);
+
+        $request = $requestBuilder->build();
+        try {
+            $result = $client->collectRates($request);
+
+        } catch(\Dhl\Express\Exception\ExpressApiException $exception) {
+            $result = Mage::getModel('shipping/rate_result');
+            $error = Mage::getModel('shipping/rate_result_error');
+            $error->setCarrier(self::CODE);
+            $error->setCarrierTitle($this->getConfigData('title'));
+            $error->setErrorMessage($this->getConfigData('specificerrmsg'));
+            $result->append($error);
+        }
+        return $result;
     }
 
     /**
---
+++ src/app/code/community/Dhl/ExpressRates/Model/Logger/Mage.php
@@ -0,0 +1,90 @@
+<?php
+/**
+ * See LICENSE.md for license details.
+ */
+
+/**
+ * Dhl_Versenden_Model_Logger_Mage
+ *
+ * @package   Dhl\ExpressRates\Model
+ * @author    Benjamin Heuer <benjamin.heuer@netresearch.de>
+ * @author    Christoph Aßmann <christoph.assmann@netresearch.de>
+ * @license   http://opensource.org/licenses/osl-3.0.php Open Software License (OSL 3.0)
+ * @link      http://www.netresearch.de/
+ */
+class Dhl_ExpressRates_Model_Logger_Mage extends \Psr\Log\AbstractLogger
+{
+    /** @var Dhl_Versenden_Model_Logger_Writer */
+    protected $_writer;
+
+    /** @var string */
+    protected $_file = 'dhl_express_rates.log';
+
+    /** @var int[] */
+    protected $_levelMapping = array(
+        \Psr\Log\LogLevel::EMERGENCY => Zend_Log::EMERG,
+        \Psr\Log\LogLevel::ALERT     => Zend_Log::ALERT,
+        \Psr\Log\LogLevel::CRITICAL  => Zend_Log::CRIT,
+        \Psr\Log\LogLevel::ERROR     => Zend_Log::ERR,
+        \Psr\Log\LogLevel::WARNING   => Zend_Log::WARN,
+        \Psr\Log\LogLevel::NOTICE    => Zend_Log::NOTICE,
+        \Psr\Log\LogLevel::INFO      => Zend_Log::INFO,
+        \Psr\Log\LogLevel::DEBUG     => Zend_Log::DEBUG,
+    );
+
+    /**
+     * Dhl_ExpressRates_Model_Logger_Mage constructor.
+     * @param Dhl_ExpressRates_Model_Logger_Writer $writer
+     * @param string $file
+     */
+    public function __construct(Dhl_ExpressRates_Model_Logger_Writer $writer, $file = '')
+    {
+        $this->_writer = $writer;
+        if ($file) {
+            $this->_file = $file;
+        }
+    }
+
+    /**
+     * Replace placeholders in context
+     * @link https://github.com/php-fig/fig-standards/blob/master/accepted/PSR-3-logger-interface.md#12-message
+     *
+     * @param $message
+     * @param mixed[] $context
+     *
+     * @return string
+     */
+    protected function interpolate($message, array $context = array())
+    {
+        // build a replacement array with braces around the context keys
+        $replace = array();
+        foreach ($context as $key => $val) {
+            // check that the value can be casted to string
+            if (!is_array($val) && (!is_object($val) || method_exists($val, '__toString'))) {
+                $replace['{' . $key . '}'] = $val;
+            }
+        }
+
+        // interpolate replacement values into the message and return
+        return strtr($message, $replace);
+    }
+
+    /**
+     * Logs with an arbitrary level.
+     *
+     * @param string $level
+     * @param string $message
+     * @param mixed[] $context
+     *
+     * @return null
+     */
+    public function log($level, $message, array $context = array())
+    {
+        if (isset($context['exception'])) {
+            $this->_writer->logException($context['exception']);
+        }
+
+        $message = $this->interpolate($message, $context);
+        $this->_writer->log($message, $this->_levelMapping[$level], $this->_file);
+    }
+}
